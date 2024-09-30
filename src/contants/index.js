5c050d307e2afedbcf2708794581ea921a23a1d3
AnhTuan020503 <151918220+AnhTuan020503@users.noreply.github.com>
2024-09-30 17:29:40 +0700

    new push

---
+++ src/contants/index.js
@@ -0,0 +1,4 @@
+import Color from "./Color";
+import images from "./images";
+import Fonts from "./Fonts";
+export {Color,images,Fonts};
---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View,StatusBar,Image } from 'react-native'
+import React from 'react'
+import { Color, images,Fonts } from '../contants'
+import { Display } from '../utils'
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+        <StatusBar barStyle="light-content" 
+        backgroundColor={Color.DEFAULT_GREEN}
+        translucent
+        />
+        <Image
+            source={images.PLATE}
+            resizeMode="contain"
+            style={styles.image}
+        />
+      <Text style={styles.titleText}>FoodDelivery</Text>
+    </View>
+  )
+}
+
+export default SplashScreen
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:Color.DEFAULT_GREEN,
+    },
+    image:{
+        height:Display.setHeight(30),
+        width:Display.setWidth(60),
+    },
+    titleText:{
+        color:Color.DEFAULT_WHITE,
+        fontSize:32,
+        // fontWeight:'bold',
+        fontFamily:Fonts.POPPINS_LIGHT,
+    },
+})
